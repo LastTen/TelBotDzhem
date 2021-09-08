5860b428edeef55a6aa609299f6ec64b1e3c94c5
LastTen <romastas1996@gmail.com>
2021-09-08 21:46:39 +0300

    ADD game. Hepy quest

--- APP.py
+++ APP.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot.types import PhotoSize
 from dataConect import dataConect
 import reg_funk
 
@@ -10,39 +11,101 @@ print('start')
 ###################################__КОМАНДИ__################
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  textStart = reg_funk.selReg(message.chat.id, message.chat.username)
-  bot.send_message(message.chat.id, textStart)
+  start_message = reg_funk.selReg(message.chat.id, message.chat.username, message.chat.first_name, message.chat.last_name)
+  bot.send_message(message.chat.id, start_message)
+
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-  bot.send_message(message.chat.id, "Для початку вибери напрямок \n /calendar \n /location")
+  bot.send_message(message.chat.id, "Для наступного кроку натисни /game \n Підказки про відповідь знаходяться на локації")
 
-@bot.message_handler(commands=['calendar'])
-def send_calendar(message):
-  bot.send_message(message.chat.id, reg_funk.calendar())
+@bot.message_handler(commands=['heppy'])
+def send_welcome(message):
+  text = f'Квест розпочинається для початку натисни /game і слідуй інструкції'
+  if message.chat.id == 469891609:
+    bot.send_message(650665525, f"{text}")
+    bot.send_location(650665525, 50.071007916968064, 31.46478637184649)
+    bot.send_message(491374102, f"{text}")
+    bot.send_location(491374102, 50.071007916968064, 31.46478637184649)
+    bot.send_message(469891609, f"{text}")
+    bot.send_location(469891609, 50.071007916968064, 31.46478637184649)
 
-@bot.message_handler(commands=['location'])
+### GAME####
+
+@bot.message_handler(commands=['game', 'Game'])
 def send_location_qwest(message):
-  bot.send_message(message.chat.id, 'Введіть порядковий номер бажаной локації')
-  bot.register_next_step_handler(message, answer_loc)
+  bot.send_message(message.chat.id, 'Для початку натисни /new_game \nдля того щоб продовжити натисни /next')
+  bot.register_next_step_handler(message, answer)
+
+def answer(message):
+  if message.text == "/next":
+    print(message.text)
+    print(message.chat.id)
+    loc = reg_funk.us_location(message)
+    bot.send_location(message.chat.id, loc[0], loc[1])
+    bot.send_message(message.chat.id, reg_funk.us_qwest(message))
+    bot.register_next_step_handler(message, answer_loc)
+  elif message.text == '/new_game':
+    reg_funk.ansv_qwest(message)
+    bot.send_message(message.chat.id, 'Ви починаєте гру спочатку відправ /next для початку')
+    bot.register_next_step_handler(message, answer)
+  elif message.text == '/stop':
+    bot.send_message(message.chat.id, 'Ви вишли з гри для старту натисни /game \nдля навігації натисни /help')
+  else:
+    print(message.text)
+    print(message.chat.id)
+    bot.send_message(message.chat.id, 'Невірний номер питання спробуй ще раз для виходу з гри натисни /stop')
+    bot.register_next_step_handler(message, answer)
+
 
 def answer_loc(message):
-  if reg_funk.send_location_li(message.text):
-    bot.send_location(message.chat.id, reg_funk.send_location_li(message.text), reg_funk.send_location_lo(message.text))
+  num_loc = reg_funk.user_answer_num(message)
+  answer = reg_funk.qwest_loc_ans(num_loc, message)
+  if answer:
+    reg_funk.next_qwest(message)
+    print(message.text)
+    print(message.chat.id)
+    loc = reg_funk.us_location(message)
+    bot.send_location(message.chat.id, loc[0], loc[1])
+    bot.send_message(message.chat.id, reg_funk.us_qwest(message))
+    bot.register_next_step_handler(message, answer_loc)
+  elif message.text == '/stop':
+    bot.send_message(message.chat.id, 'Ви вишли з гри для старту натисни /game \nдля навігації натисни /help')
   else:
-    bot.send_message(message.chat.id, 'Не вірний номер локації, спробуй ще раз \n /location')
+    print(message.text)
+    print(message.chat.id)
+    bot.send_message(message.chat.id, 'Відповідь невірна, спробуй ще раз для, виходу з гри натисни /stop')
+    bot.register_next_step_handler(message, answer_loc)
+
+
+
+
 
 
 ########################__TEXT__#####################
-@bot.message_handler(content_types= ['text'])
-def send_message(message):
-  print(message.chat.id)
-  print(message.chat.username)
-  print(message.text)
-  bot.send_message(message.chat.id, reg_funk.message_user(message.text))
+# @bot.message_handler(content_types= ['text'])
+# def send_message(message):
+#   print(message.chat.id)
+#   print(message.chat.username)
+#   print(message.text)
+#   bot.send_message(message.chat.id, reg_funk.message_user(message.text))
+# @bot.message_handler(content_types=['text'])
+# def send_message(message):
+#   bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(content_types=['text'])
+def send_welcome(message):
+    print(message.chat.id)
+    print(message.chat.username)
+    print(message.text)
+    bot.send_message(message.chat.id, 'Ви вишли з поля гри для того щоб повернутись натични /game')
+
+
+
 
 
 ####################______END______#####################
 
-bot.polling(none_stop = True , interval = 1)
+bot.polling(interval = 1)
 print('stop')
--- reg_funk.py
+++ reg_funk.py
@@ -1,6 +1,7 @@
+
+from typing import Text
 import psycopg2
 from dataConect import dataConect
-from locationSort import location
 
 
 connection = psycopg2.connect(
@@ -12,94 +13,134 @@ connection = psycopg2.connect(
 )
 
 
-def registration(idtg, nametg):
+## REGISTRATION##
+
+def registration(idtg, nametg, first_name, last_name):
   cursor = connection.cursor()
-  into = f"insert into tgusers (id, user_name) values ({idtg}, '{nametg}')"
+  into = f"insert into user_name (id, first_name, last_name, nick_name, game_loc_num) values ({idtg}, '{first_name}', '{last_name}', '{nametg}', 1)"
   cursor.execute(into)
   connection.commit()
   cursor.close()
 
-def selReg(idtg, nametg):
+def selReg(idtg, nametg, first_name, last_name):
   cursor = connection.cursor()
-  select = f"SELECT id FROM tgusers WHERE id = {idtg}"
+  select = f"SELECT id FROM user_name WHERE id = {idtg}"
   cursor.execute(select)
   if cursor.fetchall():
-    return(f"З поверненням {nametg} чим можу допомогти?")
+    return(f"З поверненням {first_name}, чим можу допомогти?")
   elif cursor.fetchone() == None:
-     registration(idtg, nametg)
+     registration(idtg, nametg, first_name, last_name)
      cursor.close()
-     return(f'Вітаю чим можу допомогти');
+     return(f'Вітаю з реєстрацією чим можу допомогти');
 
-def calendar():
+###QWEST FOR LOCATION##
+def ansv_qwest(message):
   cursor = connection.cursor()
-  cursor.execute(f"SELECT CONCAT(title, ' відбудеться  ', EXTRACT(day FROM DATE), '-', EXTRACT(month FROM date), '  детальніше...    ', category) FROM events WHERE date >= CURRENT_DATE")
-  res = ()
-  for k in cursor.fetchall():
-    res += k
+  into = f"update public.user_name set game_loc_num = 1 Where id = {message.chat.id}"
+  cursor.execute(into)
+  connection.commit()
   cursor.close()
-  return('\n'.join(res))
+  return 1
+ 
+
+
+####SEND LOCATION##
+def user_answer_num(message):
+  cursor = connection.cursor()
+  select = f"select game_loc_num from public.user_name WHERE id = {message.chat.id}"
+  cursor.execute(select)
+  res =  cursor.fetchone()[0]
+  cursor.close()
+  return res
+
+def us_location(message):
+  num_l = user_answer_num(message)
+  cursor = connection.cursor()
+  select = f"select location from public.qwestions where num_loc = {num_l}"
+  cursor.execute(select)
+  res =  cursor.fetchone()[0].split(' ')
+  cursor.close()
+  loc = []
+  for k in res:
+    loc.append(float(k))
+  return loc
+
+
+def us_qwest(message):
+  num_l = user_answer_num(message)
+  cursor = connection.cursor()
+  select = f"select qwest from public.qwestions where num_loc = {num_l}"
+  cursor.execute(select)
+  res =  cursor.fetchone()[0]
+  cursor.close()
+  return res
+
+def qwest_loc_ans(num_loc, message):
+  text = f"{message.text}".upper()
+  cursor = connection.cursor()
+  select = f"select answer from public.qwestions  where num_loc = {num_loc} and answer = '{text}'"
+  cursor.execute(select)
+  res = cursor.fetchone()
+  cursor.close()
+  return res
 
 
 
-def send_location_li(num_location):
-  for k in location:
-    if k == num_location:
-      return location[k]['li']
-    
-
-def send_location_lo(num_location):
-  for k in location:
-    if k == num_location:
-      return location[k]['lo']
+def next_qwest(message):
+  cursor = connection.cursor()
+  into = f"update public.user_name set game_loc_num = game_loc_num + 1 Where id = {message.chat.id}"
+  cursor.execute(into)
+  connection.commit()
+  cursor.close()
 
 
 
-#### ОБРОБКА ТЕКСТА #####
-def qwest_user(text):
-  message_qwest = []
-  text_user = text.lower().replace('?', '').replace('.', '').replace(',', '')
-  list_text = text_user.split(' ')
-  for len_text in list_text:
-    if len(len_text) > 3:
-      message_qwest.append(len_text)
-  return message_qwest
+# #### ОБРОБКА ТЕКСТА #####
+# def qwest_user(text):
+#   message_qwest = []
+#   text_user = text.lower().replace('?', '').replace('.', '').replace(',', '')
+#   list_text = text_user.split(' ')
+#   for len_text in list_text:
+#     if len(len_text) > 3:
+#       message_qwest.append(len_text)
+#   return message_qwest
 
 
-def message_category(text):
-  qwest = qwest_user(text)
-  message_answer = []
-  for key in qwest:
-    if key[0] == '/':
-      cursor = connection.cursor()
-      cursor.execute("SELECT category_name, description FROM category")
-      category_data = cursor.fetchall()
-      cursor.close()
-      for n_category in category_data:
-        if n_category[0] == key:
-          message_answer.append(n_category[1])
-  return message_answer
+# def message_category(text):
+#   qwest = qwest_user(text)
+#   message_answer = []
+#   for key in qwest:
+#     if key[0] == '/':
+#       cursor = connection.cursor()
+#       cursor.execute("SELECT category_name, description FROM category")
+#       category_data = cursor.fetchall()
+#       cursor.close()
+#       for n_category in category_data:
+#         if n_category[0] == key:
+#           message_answer.append(n_category[1])
+#   return message_answer
   
 
-def message_text(text):
-  qwest = qwest_user(text)
-  message_answer = []
-  for key in qwest:
-    cursor = connection.cursor()
-    like_qwest = f"'{key}%'"
-    cursor.execute(f"SELECT answer FROM answer WHERE qwest LIKE {like_qwest}")
-    answer_text = cursor.fetchall()
-    cursor.close()
-    if answer_text != []:
-      message_answer.append(answer_text[0][0])
-  return(message_answer)
+# def message_text(text):
+#   qwest = qwest_user(text)
+#   message_answer = []
+#   for key in qwest:
+#     cursor = connection.cursor()
+#     like_qwest = f"'{key}%'"
+#     cursor.execute(f"SELECT answer FROM answer WHERE qwest LIKE {like_qwest}")
+#     answer_text = cursor.fetchall()
+#     cursor.close()
+#     if answer_text != []:
+#       message_answer.append(answer_text[0][0])
+#   return(message_answer)
 
 
-def message_user(text):
-  category = message_category(text)
-  answer = message_text(text)
-  gener_answer = answer + category
-  if len(gener_answer) > 0:
-    return"\n".join(gener_answer)
-  else:
-    return "Не зрозумів питання для допомлги натисни /help"
+# def message_user(text):
+#   category = message_category(text)
+#   answer = message_text(text)
+#   gener_answer = answer + category
+#   if len(gener_answer):
+#     return"\n".join(gener_answer)
+#   else:
+#     return "Не зрозумів питання для допомлги натисни /help"
 
